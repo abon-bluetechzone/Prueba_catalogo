65d33fe7ac2a2e19121ddae247da11ef8399ddba
abon-bluetechzone <antonio.bon@bluetechzone.com>
2025-08-06 08:53:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function filterApps(area) {
+  const items = document.querySelectorAll('.app-item');
+  items.forEach(item => {
+    if (area === 'all' || item.classList.contains(area)) {
+      item.classList.remove('hidden');
+    } else {
+      item.classList.add('hidden');
+    }
+  });
+}
