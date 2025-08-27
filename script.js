d737c0d5c52bdf3c54c62ed3eb75c7d4b7dc3e1f
abon-bluetechzone <antonio.bon@bluetechzone.com>
2025-08-27 12:41:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function filterApps(area) {
   const items = document.querySelectorAll('.app-item');
   items.forEach(item => {
-    if (area === 'all' || item.classList.contains(area)) {
+    if (area === 'all' || area === 'todos' || item.classList.contains(area)) {
       item.classList.remove('hidden');
     } else {
       item.classList.add('hidden');
